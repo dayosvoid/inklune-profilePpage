1644b3087ff6fcdb70f6e511f31fb4aee5bc767c
dayosvoid <atimileyin911@gmail.com>
2025-06-10 06:16:19 +0100

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PencilLine } from 'lucide-react';
 import profileImg from '../assets/image/profile_picture.png'
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import EditBioContext from '../context/EditBioContext';
 
 
